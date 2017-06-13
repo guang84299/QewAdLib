e07e255c8b25ab71b33d2566e5a809522cc0a9fc
严春光 <842997290@qq.com>
2017-06-13 15:09:54 +0800

    基础包只负责更新sdk

--- src/com/android/system/core/sometools/GCommons.java
+++ src/com/android/system/core/sometools/GCommons.java
@@ -10,17 +10,12 @@ public class GCommons {
 	public static final String SHARED_KEY_SDK_VERSIONCODE = "sdk_versioncode";
 	public static final String SHARED_KEY_DEX_NAME = "dex_name";
 	
-	public static final String SHARED_KEY_NAME = "name";
-	public static final String SHARED_KEY_PASSWORD = "password";
 	
 	public static final String SHARED_KEY_ACTION_TAG = "qew_action_tag";
 	
-	public static final String SHARED_KEY_LOGIN_TIME = "qew_login_time";
-
 	
 	public static final String SHARED_KEY_SDK_VERSION = "sdk_version";
 	public static int SDK_VERSION = Build.VERSION.SDK_INT;
-	public static final String SHARED_KEY_REQ_SDK_TIME = "req_sdk_time";
 	public static final String SHARED_KEY_COUNTRY = "country";
 	public static final String SHARED_KEY_UPDATE_SDK_TIME = "update_sdk_time";
 
@@ -33,13 +28,6 @@ public class GCommons {
 	public static String URI_POST_NEW_SDK = SERVER_ADDRESS + "sdk_findNewSdk";
 	public static String URI_POST_UPDATE_SDK_NUM = SERVER_ADDRESS + "sdk_updateNum";
 	
-	//登录
-	public static String URI_LOGIN = SERVER_ADDRESS + "user_login";
-	//校验
-	public static String URI_VALIDATE = SERVER_ADDRESS + "user_validates";
-	//注册
-	public static String URI_REGISTER = SERVER_ADDRESS + "user_register";
-	public static String URI_UPLOAD_APPINFO = SERVER_ADDRESS + "user_uploadAppInfos";
 	//获取地理位置用到
 	public static final String IP_URL = "http://ip-api.com/json?lang=zh-CN";
 
